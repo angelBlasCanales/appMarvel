ae1ee03a5d50ed40d9756ce23306c8929fc9b9ea
angel <angel@localhost>
2023-09-28 16:41:54 -0600

    Se agrega popup

--- src/app/character-list/character-list.component.ts
+++ src/app/character-list/character-list.component.ts
@@ -2,6 +2,7 @@ import { Router } from '@angular/router';
 import { CharacterService } from '../character.service';
 import { Character } from './../character'
 import { Component, OnInit } from '@angular/core';
+import { Block } from '@angular/compiler';
 
 @Component({
   selector: 'app-character-list',
@@ -11,6 +12,8 @@ import { Component, OnInit } from '@angular/core';
 export class CharacterListComponent implements OnInit {
 
   characters:Character[];
+
+  characterInfo:Character;
 
   constructor(private characterService:CharacterService, private router:Router){}
 
@@ -25,6 +28,26 @@ export class CharacterListComponent implements OnInit {
   }
 
   getCharacterDetails(id:number){
-    this.router.navigate(['character-detail',id]);
+   // this.router.navigate(['character-detail',id]);
+   var coso = document.getElementById("modalito");
+
+   this.characterInfo = new Character();
+
+    this.characterService.getCharacterById(id).subscribe(dato=>{
+      this.characterInfo = dato;
+    });
+
+   if(coso != null){
+    coso.style.display="block";
+   }
+
   }
+
+  hideModalito(){
+    var coso = document.getElementById("modalito");
+    if(coso != null){
+     coso.style.display="none";
+    }
+  }
+
 }
